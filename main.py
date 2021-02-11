3b9317523ce25f101bff0004caeb70fb729058dc
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-11 06:08:45 +0000

    Add initial setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Instance of Tk
+interface = Tk()
+
+# Default settings
+interface.title("Miles to Kilometer Converter")
+interface.minsize(width=500, height=300)
+
+# Keep the interface open until exited
+interface.mainloop()
+
