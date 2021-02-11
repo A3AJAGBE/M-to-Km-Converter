3f09e78401e8867a6358c6cc5dcc592eff244602
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-11 07:01:33 +0000

    Add convert function

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from tkinter import *
 
+
+def m_to_km():
+    """This function converts the meter input to kilometer"""
+    meter = float(m_input.get())
+    kilometer = meter / 1000
+    km_input.delete('0', 'end')
+    km_input.insert(END, string=kilometer)
+
+
 # Instance of Tk
 interface = Tk()
 
@@ -19,14 +28,18 @@ equal_label = Label(text="=", font=("Arial", 24, "bold"))
 equal_label.grid(column=1, row=0)
 
 km_input = Entry(width=10)
+km_input.insert(END, '0')
 km_input.grid(column=2, row=0)
+
+# km_result = Label(text=" ", font=("Arial", 24, "bold"))
+# km_result.grid(column=2, row=0)
 
 km_label = Label(text="Kilometer", font=("Arial", 18, "normal"))
 km_label.grid(column=2, row=1)
 
-calc_button = Button(text="Calculate")
+calc_button = Button(text="Calculate", command=m_to_km)
 calc_button.grid(column=1, row=3)
+calc_button.config(padx=5, pady=5)
 
 # Keep the interface open until exited
 interface.mainloop()
-
